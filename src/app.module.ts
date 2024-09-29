1d48123d3bf9d61ac2870e798c77667c5311a300
solux <loctranphuoc456@protonmail.com>
2024-09-29 15:26:05 +0700

    CORS setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WebsocketModule } from './websocket/websocket.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), WebsocketModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { SocketIOAdapter } from './shared/adapters/socket-io.adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get<ConfigService>(ConfigService);
+
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
+
+  app.useWebSocketAdapter(new SocketIOAdapter(app, configService));
+
   await app.listen(3000);
 }
 bootstrap();
